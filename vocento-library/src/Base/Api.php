8d1b3844a6e2b3bf6f893da29f26585a22875822
ioedumac <eacevedof@yahoo.es>
2020-03-30 13:31:12 +0200

    api provider ok

--- vocento-library/src/Base/Api.php
+++ vocento-library/src/Base/Api.php
@@ -1,25 +1,49 @@
 <?php
 namespace League\Base;
 
-final class Api
+class Api
 {
-    const ENDPOINT = "http://localhost:3600";
+    const DEFAULT_ENDPOINT = "http://localhost:3600";
+    const DEFAULT_TYPE = "fixture";
     
+    private $url = "";
+    private $type = "";
     
-    public function _get_response()
+    /**
+     * Customizar el endpoint  
+     * @param type $url unico endpoint
+     * @param type $type el set: fixture|match
+     */
+    public function __construct($url=self::DEFAULT_ENDPOINT,$type= self::DEFAULT_TYPE) 
     {
-        
+        $this->url = $url;
+        $this->type = $type;
     }
-
-
-    public function get_fixtures($date,$team):array
+ 
+    private function _get_response($url)
     {
-        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL,$url);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result, true);  
     }
     
-    public function get_match_report()
+    public function get_data()
     {
-        
+        $url = $this->url."?set=$this->type";
+        return $this->_get_response($url);
+    }
+    
+    public function set_endpoint($url= self::DEFAULT_ENDPOINT)
+    {
+        $this->url = $url;
+    }
+    
+    public function set_type($type=self::DEFAULT_TYPE)
+    {
+        $this->type = $type;
     }
     
 }
--- vocento-library/src/Base/Base.php
+++ vocento-library/src/Base/Base.php
@@ -7,9 +7,19 @@ abstract class Base
 {
     protected $objapi;
     
-    public function __construct(Api $objapi)
+    public function __construct()
     {
-        $this->objapi = $objapi;
+        $this->objapi = new Api();
     }
 
+    public function get_fixtures()
+    {
+        $this->objapi->set_type("fixtures");
+                
+    }
+    
+    public function get_match()
+    {
+        
+    }
 }
