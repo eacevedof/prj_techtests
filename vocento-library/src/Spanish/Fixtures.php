1d811a4a730d8a0ac0a2ee1e03c2720d8820dcec
ioedumac <eacevedof@yahoo.es>
2020-03-30 14:04:29 +0200

    api fixtures

--- vocento-library/src/Spanish/Fixtures.php
+++ vocento-library/src/Spanish/Fixtures.php
@@ -1,30 +1,49 @@
 <?php
 namespace League\Spanish;
 
+use League\Base\Base;
 use League\Base\ICommon;
-use League\Base\Api;
 
-class Fixtures implements ICommon
+class Fixtures extends Base implements ICommon
 {
-   
+    private $teamid;
+    
+    public function __construct($teamid) 
+    {
+        //conector api
+        parent::__construct();
+        $this->teamid = $teamid;
+        
+    }
+    
+    private function _load_raw()
+    {
+        $this->objapi->add_param("date", $this->date)->add_param("teamid", $this->teamid);
+        $this->data = $this->get_fixtures();        
+    }
+    
+    public function get_teams($date)
+    {
+        return $this->data[$date]["teams"];
+    }    
+    
+    public function get_location() 
+    {
+        return $this->data[$date]["teams"];
+    }    
+    
     public function get_datetime() 
     {
         
     }
 
-    public function get_location() 
-    {
-        
-    }
+ 
 
     public function get_result() 
     {
-        
+        return $this->data["result"];
     }
 
-    public function get_teams() 
-    {
-        
-    }
+
 
 }
