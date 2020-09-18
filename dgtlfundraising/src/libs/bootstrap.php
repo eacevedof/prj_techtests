23d175fa628fa65a2488c958fef636d3e0c41a5d
ioedumac <eacevedof@yahoo.es>
2020-09-18 12:41:34 +0200

    bootsrap ok

--- dgtlfundraising/src/libs/bootstrap.php
+++ dgtlfundraising/src/libs/bootstrap.php
@@ -1,5 +1,3 @@
 <?php
-include_once("/db/ComponentMysql.php");
-include_once("./actions/Actividad1.php");
-include_once("./actions/Actividad2.php");
-include_once("./actions/Actividad3.php");
+include_once("db/ComponentMysql.php");
+include_once("actions/Actividad1.php");
