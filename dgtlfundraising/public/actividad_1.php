aa80c413bdcdbd5c5518ce1323eb2c95c80dd6e0
ioedumac <eacevedof@yahoo.es>
2020-09-18 12:33:54 +0200

    layout ok

--- dgtlfundraising/public/actividad_1.php
+++ dgtlfundraising/public/actividad_1.php
@@ -1,1 +1,6 @@
 <?php
+
+
+
+include("../src/layout/layout-top.php");
+include("../src/layout/layout-bottom.php");
--- dgtlfundraising/src/layout/layout-top.php
+++ dgtlfundraising/src/layout/layout-top.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title><?= $pagetitle ?? "" ?></title>
+    <title></title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,6 +21,3 @@
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </head>
 <body>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-</html>
