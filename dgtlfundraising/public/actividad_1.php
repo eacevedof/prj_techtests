bc1ac55b46e4d59a0050002ae23cf62d86fec01a
ioedumac <eacevedof@yahoo.es>
2020-09-18 12:56:57 +0200

    form ok

--- dgtlfundraising/public/actividad_1.php
+++ dgtlfundraising/public/actividad_1.php
@@ -4,21 +4,36 @@ include("../src/libs/bootstrap.php");
 <?php
 include("../src/layout/layout-top.php");
 ?>
-<form>
+<main class="container">
+<h2> Actividad 1 </h2>
+<form method="post" action="/actividad_1.php">
     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label for="titulo" class="form-label">Titulo</label>
+        <input type="text" class="form-control" id="titulo" name="titulo">
     </div>
     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label for="director" class="form-label">Director</label>
+        <input type="text" class="form-control" id="director" name="director">
     </div>
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <div class="mb-3">
+        <label for="anio" class="form-label">Año</label>
+        <input type="text" class="form-control" id="anio" name="anio">
+    </div>
+    <div class="mb-3">
+        <label for="pais" class="form-label">Pais</label>
+        <select class="form-control" id="pais" name="pais">
+            <option value="">Select one</option>
+        <?php
+        foreach ($paises as $code =>$pais){
+        ?>
+        <option value="<?= $code?>"><?= $pais ?></option>
+        <?php
+        }
+        ?>
+        </select>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+</main>
 <?php
 include("../src/layout/layout-bottom.php");
