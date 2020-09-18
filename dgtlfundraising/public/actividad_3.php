f27cd8f8cb56ca1c02a50e9d6a06c460fcceead9
ioedumac <eacevedof@yahoo.es>
2020-09-18 15:16:23 +0200

    actividad3 con menu

--- dgtlfundraising/public/actividad_3.php
+++ dgtlfundraising/public/actividad_3.php
@@ -3,12 +3,17 @@
 include("src/libs/bootstrap.php");
 
 include("src/layout/layout-top.php");
-
+?>
+<main class="container">
+<h2>Actividad 3 - Calculadora</h2>
+<?php
 $numero1 = 10;
 $numero2 = 5;
 $calculadora = new \Libs\Actions\Calculadora();
 $calculadora->get($numero1, $numero2);
-
+?>
+</main>
+<?php
 include("src/layout/layout-bottom.php");
 ?>
 
