15cb68b7c1bf20c66a0ce462260f60f95cffa61c
ioedumac <eacevedof@yahoo.es>
2020-09-18 14:52:01 +0200

    get_conuntryname

--- dgtlfundraising/public/actividad_2.php
+++ dgtlfundraising/public/actividad_2.php
@@ -4,37 +4,46 @@ include("src/libs/bootstrap.php");
 $actividad2 = new \Libs\Actions\Actividad2([]);
 $r = $actividad2->get_peliculas();
 
-
 include("src/layout/layout-top.php");
 ?>
+<style>
+.td-border{
+    border: 1px solid black !important;
+}
+.th-bg{
+    background-color: #ccc !important;
+}
+.td-bg{
+    background-color: #3ebad8 !important;
+}
+</style>
 <main class="container">
 <h2> Actividad 2 </h2>
 <h3>Número total de películas encontradas: <?= count($r); ?></h3>
 <table class="table">
     <thead>
-        <th>Titulo</th>
-        <th>Director</th>
-        <th>Año</th>
-        <th>País</th>
+        <th class="td-border th-bg">Titulo</th>
+        <th class="td-border th-bg">Director</th>
+        <th class="td-border th-bg">Año</th>
+        <th class="td-border th-bg">País</th>
     </thead>
     <tbody>
 <?php
 foreach($r as $i => $row):
-
+    $bgcolor = "";
+    if(($i%2)!==0) $bgcolor = "td-bg";
 ?>
     <tr>
-        <td><?= $row["titulo"]; ?></td>
-        <td><?= $row["director"]; ?></td>
-        <td><?= $row["anio"]; ?></td>
-        <td><?= $row["pais"]; ?></td>
+        <td class="td-border <?=$bgcolor;?>"><?= $row["titulo"]; ?></td>
+        <td class="td-border <?=$bgcolor;?>"><?= $row["director"]; ?></td>
+        <td class="td-border <?=$bgcolor;?>"><?= $row["anio"]; ?></td>
+        <td class="td-border <?=$bgcolor;?>"><?= get_countryname($row["pais"]); ?></td>
     </tr>
 <?php
 endforeach;
 ?>
     </tbody>
 </table>
-<script>
-</script>
 </main>
 <?php
 include("src/layout/layout-bottom.php");
