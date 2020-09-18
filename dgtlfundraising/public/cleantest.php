4b556d1b2a64d96c3c94eb8e96c4a0e10b841c9c
ioedumac <eacevedof@yahoo.es>
2020-09-18 16:03:44 +0200

    cleaner

--- dgtlfundraising/public/cleantest.php
+++ dgtlfundraising/public/cleantest.php
@@ -2,7 +2,7 @@
 //http://actividades.dgtlfundraising.com/eduardoacevedo/cleantest.php
 include("src/libs/bootstrap.php");
 $db = new \Lig\Db\ComponentMysql();
-$sql = "DELETE FROM peliculas WHERE titulo=''";
+$sql = "DELETE FROM peliculas WHERE titulo='' OR titulo LIKE '%prueba%' ";
 $r = $db->exec($sql);
 echo "$sql\n<br/>";
 var_dump($r);
