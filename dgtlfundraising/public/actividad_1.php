bd14b22c731b48becebe0878abc8f536c3d10a64
ioedumac <eacevedof@yahoo.es>
2020-09-18 14:18:56 +0200

    mensaje

--- dgtlfundraising/public/actividad_1.php
+++ dgtlfundraising/public/actividad_1.php
@@ -8,7 +8,7 @@ if(!empty($_POST)) {
    if(isset($_POST["titulo"])) {
         $actividad1 = new \Libs\Actions\Actividad1($_POST);
         $r = $actividad1->save();
-        $ok = "Datos guardados";
+        $ok = "Los datos se han guardado correctamente.";
         $_POST = [];
    }
    else{
@@ -42,15 +42,15 @@ include("src/layout/layout-top.php");
     ?>
     <div class="mb-3">
         <label for="titulo" class="form-label">Titulo</label>
-        <input type="text" class="form-control" id="titulo" name="titulo" maxlength="255" value="<?=$_POST["titulo"] ?? "" ?>">
+        <input type="text" class="form-control" id="titulo" name="titulo" maxlength="255" >
     </div>
     <div class="mb-3">
         <label for="director" class="form-label">Director</label>
-        <input type="text" class="form-control" id="director" name="director" maxlength="255" value="<?=$_POST["director"] ?? "" ?>">
+        <input type="text" class="form-control" id="director" name="director" maxlength="255" >
     </div>
     <div class="mb-3">
         <label for="anio" class="form-label">Año</label>
-        <input type="number" class="form-control" id="anio" name="anio" max="9999" value="<?= $_POST["anio"] ?? 0 ?>">
+        <input type="number" class="form-control" id="anio" name="anio" max="9999" >
     </div>
     <div class="mb-3">
         <label for="pais" class="form-label">Pais</label>
