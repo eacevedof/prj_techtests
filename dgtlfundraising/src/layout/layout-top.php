5486e7585f5130d68c52fdfe13ffca9fc3a5f796
ioedumac <eacevedof@yahoo.es>
2020-09-18 12:45:33 +0200

    paises inc ok

--- dgtlfundraising/src/layout/layout-top.php
+++ dgtlfundraising/src/layout/layout-top.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title></title>
+    <title>Prueba Técnica</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,7 +15,7 @@
     <meta name="security" content="public" />
     <meta http-equiv="Cache-Control" content="s-maxage = 3600, max-age = 60, private" />
 
-    <link rel="icon" type="image/png" href="https://resources.theframework.es/eduardoaf.com/20200917/161342-favicon.ico"/>
+    <link rel="icon" type="image/png" href=""/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
--- dgtlfundraising/src/libs/bootstrap.php
+++ dgtlfundraising/src/libs/bootstrap.php
@@ -1,3 +1,4 @@
 <?php
 include_once("db/ComponentMysql.php");
 include_once("actions/Actividad1.php");
+include_once("vendor/paises.inc");
