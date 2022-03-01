f2a9a7ac1109f46b31508636441a2492f9789b79
ioedumac <eacevedof@yahoo.es>
2022-03-01 18:42:12 +0100

    fix index orig

--- sgi-phpmysql/app/index.php
+++ sgi-phpmysql/app/index.php
@@ -12,21 +12,21 @@ function dd($var, $title="")
     die;
 }
 
-error_reporting( 0 );
+//error_reporting( 0 );
 spl_autoload_register( function( $className ) {
     $className = str_replace( "\\", DIRECTORY_SEPARATOR, $className );
-    include_once __DIR__ . 'index.php/' . $className . '.php';
+    include_once __DIR__. '/' . $className . '.php';
 });
 
 $ctrl = new \controllers\Site();
+
 if ( !empty( $_GET[ "action" ] ) ) {
     $action = "action".ucwords( $_GET[ "action" ] );
 } else {
     $action = "action".ucwords( $ctrl->getDefaultAction() );
 }
-dd($_GET);
 if ( !empty( $ctrl ) ) {
-    
+
     if ( method_exists( $ctrl, $action ) ) {
         $ctrl->$action();
     } else {
