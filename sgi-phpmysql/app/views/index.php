fb5af2dc64a18bbed6914378943ddd81fba03f9d
ioedumac <eacevedof@yahoo.es>
2022-03-01 22:54:13 +0100

    confirm

--- sgi-phpmysql/app/views/index.php
+++ sgi-phpmysql/app/views/index.php
@@ -9,52 +9,50 @@
         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </head>
     <body>
-        <div class="container">
-            <h3>
-              <a class="link" href="/">Productos</a>
+    <div class="container">
+        <h3>
+          <a class="link" href="/">Productos</a>
 <!--
 @eaf
-  convierto a friendly url y agrego a productos un enlace para refresh y apunto al metodo principal actionProductos
+convierto a friendly url y agrego a productos un enlace para refresh y apunto al metodo principal actionProductos
 -->
-              <a class="btn btn-success" href="/productos">Nuevo producto</a>
-              <a class="btn btn-primary" href="/xml" target="_blank">XML</a>
-            </h3>
-            
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nombre</th>
-                        <th>Descripción</th>
-                        <th>Imagen</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php if ( !empty( $models[ "productos"  ] ) ) : ?>
-                        <?php foreach ( $models[ "productos"  ] as $p ) : ?>
-                            <tr>
-                                <td><?= htmlentities( $p->getId() ) ?></td>
-                                <td><?= htmlentities( $p->getNombre() ) ?></td>
-                                <td><?= htmlentities( $p->getDescripcion() ) ?></td>
-                                <td><img src="/productos/<?= $p->getId() ?>?image=1" /></td>
-                                <td>
-                                    <a class="btn btn-primary" href="/productos/<?= $p->getId() ?>">Editar</a>
-                                    <button type="button" class="btn btn-danger" onclick="deleteConfirm(`/productos/<?= $p->getId() ?>/delete`)">
-                                      Eliminar
-                                    </button>
-                                    <a class="btn btn-dark" href="/productos/<?= $p->getId() ?>/xml">Xml</a>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <tr><td colspan="5">No hay productos.</td></tr>
-                    <?php endif; ?>
-                </tbody>
-            </table>
-        </div>
+          <a class="btn btn-success" href="/productos">Nuevo producto</a>
+          <a class="btn btn-primary" href="/xml" target="_blank">XML</a>
+        </h3>
 
-
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Imagen</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if ( !empty( $models[ "productos"  ] ) ) : ?>
+                    <?php foreach ( $models[ "productos"  ] as $p ) : ?>
+                        <tr>
+                            <td><?= htmlentities( $p->getId() ) ?></td>
+                            <td><?= htmlentities( $p->getNombre() ) ?></td>
+                            <td><?= htmlentities( $p->getDescripcion() ) ?></td>
+                            <td><img src="/productos/<?= $p->getId() ?>?image=1" /></td>
+                            <td>
+                                <a class="btn btn-primary" href="/productos/<?= $p->getId() ?>">Editar</a>
+                                <button type="button" class="btn btn-danger" onclick="deleteConfirm(`/productos/<?= $p->getId() ?>/delete`)">
+                                  Eliminar
+                                </button>
+                                <a class="btn btn-dark" href="/productos/<?= $p->getId() ?>/xml">Xml</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr><td colspan="5">No hay productos.</td></tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
     <script>
       function deleteConfirm(url){
         Swal.fire({
