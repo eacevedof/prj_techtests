703e01f47c28a2b6fbca732bbb13904f26fef37a
ioedumac <eacevedof@yahoo.es>
2022-03-02 20:16:38 +0100

    categorias ya crea

--- sgi-phpmysql/app/views/producto.php
+++ sgi-phpmysql/app/views/producto.php
@@ -2,34 +2,34 @@
     <head>
         <meta charset="UTF-8">
         <title>Productos</title>
-      <?
-      include("assets.php");
-      ?>
+    <?
+    include("assets.php");
+    ?>
     </head>
     <body>
         <div class="container">
             <h3>
               <a class="link" href="/">Productos</a>
             </h3>
-            <?php if ( !empty( $models[ "producto" ] ) ) : ?>
-                <?php
-                $id = $models[ "producto" ]->getId();
-                ?>
+            <?php
+            if ( !empty( $models["producto"] ) ) :
+                $id = $models["producto"]->getId();
+            ?>
                 <h3>Producto: <?= ( !empty( $id ) ? $id : "Nuevo" ) ?></h3>
 
                 <form class="form" method="POST" enctype="multipart/form-data">
                     <?php if ( !empty( $models[ "error" ] ) ) : ?>
                         <div class="alert alert-danger"><?= htmlentities( $models[ "error" ] ) ?></div>
                     <?php endif; ?>
-                    <input type="hidden" name="id" value="<?= $models[ "producto" ]->getId() ?>" />
+                    <input type="hidden" name="id" value="<?= $models["producto"]->getId() ?>" />
                     <input type="hidden" name="action" value="save" />
                     <div class="form-group">
                         <label for="nombre" class="control-label">Nombre</label>
-                        <input class="form-control" autofocus maxlength="162" type="text" id="nombre" name="nombre" value="<?= htmlentities( $models[ "producto" ]->getNombre() ) ?>" />
+                        <input class="form-control" autofocus maxlength="162" type="text" id="nombre" name="nombre" value="<?= htmlentities( $models["producto"]->getNombre() ) ?>" />
                     </div>
                     <div class="form-group">
                         <label for="descripcion" class="control-label">Descripción</label>
-                        <textarea class="form-control" maxlength="258" id="descripcion" name="descripcion"><?= htmlentities( $models[ "producto" ]->getDescripcion() ) ?></textarea>
+                        <textarea class="form-control" maxlength="258" id="descripcion" name="descripcion"><?= htmlentities( $models["producto"]->getDescripcion() ) ?></textarea>
                     </div>
                     <div class="form-group">
                         <label for="imagen" class="control-label">Imagen</label>
